4ec5db9ea80abd0d8922eeffae3ef8ed30663733
Ziga <zk1148@student.uni-lj.si>
2016-03-08 13:28:01 +0000

    dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -6,7 +6,20 @@ window.addEventListener('load', function() {
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
 	}
 	
+	var dodajOpomnik = function(event){
+		 var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		 var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		 document.querySelector("#naziv_opomnika").value="";
+		 document.querySelector("#cas_opomnika").value="";
+		 
+		 document.querySelector("#opomniki").innerHTML += "<div class='opomnik'> \
+            <div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
+            <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> \
+			</div>"
+	}
+	
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
