e80ac168017f8243e74f1e92ea3a08a30984165f
Ziga <zk1148@student.uni-lj.si>
2016-03-08 13:03:06 +0000

    implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	var izvediPrijavo = function(event){
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}
+	
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
